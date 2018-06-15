7fd17b71265cdd849616911238c5d676c2970656
Toby Merlet <toby.merlet@kitegb.com>
2018-06-15 11:00:24 +0100

    simple test passing

--- src/sum/index.js
+++ src/sum/index.js
@@ -1,3 +1,8 @@
-const sum = (a, b) => a + b;
+const add = (...args) => ({
+    value: () => [...args].reduce((acc, curr) => acc + curr),
+    add: (...args2) => ({
+        value: () => [...args].reduce((acc, curr) => acc + curr) + [...args2].reduce((acc, curr) => acc + curr)
+    })
+});
 
-export default sum;
+export default add;
--- src/sum/index.test.js
+++ src/sum/index.test.js
@@ -1,5 +1,5 @@
-import sum from './';
+import add from './';
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toEqual(3);
+    expect(add(1, 2, 3).add(5).value()).toEqual(11);
 });
