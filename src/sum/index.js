459007b56ef97b511b6c26efb516054f89f57822
Toby Merlet <toby.merlet@kitegb.com>
2018-06-16 10:42:21 +0100

    karma test

--- src/sum/index.js
+++ src/sum/index.js
@@ -1,8 +1,30 @@
-const add = (...args) => ({
-    value: () => [...args].reduce((acc, curr) => acc + curr),
-    add: (...args2) => ({
-        value: () => [...args].reduce((acc, curr) => acc + curr) + [...args2].reduce((acc, curr) => acc + curr)
-    })
-});
+const add = (...args) => {
+    return ({
+        value: () => [...args].reduce((acc, curr) => acc + curr),
+        add: (...args2) => add(...args, ...args2)
+    });
+};
 
 export default add;
+
+//  EXAMPLE WITH A CLASS
+//
+// class Add {
+//     constructor(nums, startingTotal = 0) {
+//         this.total = nums.reduce((total, n) => total + n, startingTotal);
+//     }
+
+//     value() {
+//         return this.total;
+//     }
+
+//     add(...nums) {
+//         return new Add(nums, this.value());
+//     }
+// }
+
+// const add = (...nums) => {
+//     return new Add(nums);
+// };
+
+// export default add;
--- src/sum/index.test.js
+++ src/sum/index.test.js
@@ -1,5 +1,5 @@
 import add from './';
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2, 3).add(5).value()).toEqual(11);
+    expect(add(1, 2, 3).add(5).add(10).value()).toEqual(21);
 });
